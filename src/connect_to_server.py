3798392ca53c5ed9bb7f457d4221ad447f2930d9
Erin Pryor <pryor.e@northeastern.edu>
2026-02-15 15:47:43 -0500

    updated a few things

--- src/connect_to_server.py
+++ src/connect_to_server.py
@@ -4,7 +4,8 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import credentials
 import pandas as pd
-
+import matplotlib.pyplot as plt
+#Used Vans as a template for now - where i got the sys info from)
 #set up connections. note: the CREDENTIALS.py file is NOT to be pushed 
 connection = redshift_connector.connect(
      host=credentials.HOST,
@@ -18,13 +19,20 @@ def attempt1():
 
     query = f'''
             SELECT *
-            FROM {credentials.SCHEMAEP}.eda 
+            FROM {credentials.SCHEMA}.eda_vascular_dementia 
         
         '''
     cursor.execute(query)
     df = cursor.fetch_dataframe()
+    df['Age']=2026-df['year_of_birth']
     return df
 
 df=attempt1()
-print(df.head)
+#print(df.head)
+
+# df.boxplot(column=['Age'], by='gender_concept_id', grid=False)# 
+# plt.show()
+
+print(df.columns)
+
  
