e30316d64e9de602dc77441a7cb3cfbbfdf8d7e5
Ryan Webb <102746267+RyanFWebb@users.noreply.github.com>
2026-02-21 13:18:24 -0500

    Fix import statement for credentials module

--- src/dementia_set.py
+++ src/dementia_set.py
@@ -2,7 +2,7 @@ import redshift_connector
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from src import credentials
+import credentials
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
